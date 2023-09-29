7d7c723a1a179decd5c6306869350aba5041d036
CarloCassini <c.cassini87@gmail.com>
2023-09-29 16:07:46 +0200

    collegati i primi 2 oggetti

--- db.php
+++ db.php
@@ -1,4 +1,11 @@
 <?php
 
+include __DIR__ . "./Models/Movie.php";
+
+$movie_1 = new Movie();
+$movie_1->title = "il signore degli anelli";
+
+$movie_2 = new Movie();
+$movie_2->language = "italiano";
 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+include __DIR__ . "./db.php";
 
-?>
+var_dump($movie_1);
+var_dump($movie_2)
+    ?>
