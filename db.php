336ead4289cdc4ae14ce9154e84a4c883cac2bd6
CarloCassini <c.cassini87@gmail.com>
2023-09-29 23:29:55 +0200

    non sono sicuro di essere riuscito

--- db.php
+++ db.php
@@ -3,10 +3,12 @@
 include __DIR__ . "./Models/Movie.php";
 include __DIR__ . "./Models/Genere.php";
 
-$categoria_2 = new Genere("Aabbano");
 $categoria_1 = new Genere("azzurro");
+$categoria_2 = new Genere("Aabbano");
+$array_categorie = [$categoria_1, $categoria_2];
+var_dump($array_categorie);
 
-$movie_1 = new Movie("ciccio", new Genere("Spavento"), 120, "dell'amore", 4, );
+$movie_1 = new Movie("ciccio", $array_categorie, 120, "dell'amore", 4, );
 $movie_1->test = "eccolo";
 // $movie_1->title = "il signore degli anelli";
 
